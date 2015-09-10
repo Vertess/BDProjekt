87a36d8ea5c91c15dc475d8a9e9416b1e93e5e43
lwieserce <lwieserce114@gmail.com>
2015-09-10 10:20:00 +0200

    Added DostawaControl

---
+++ BDProjekt/Controls/DostawaControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace BDProjekt.Controls
+{
+    public partial class DostawaControl : UserControl
+    {
+        public DostawaControl()
+        {
+            InitializeComponent();
+        }
+        private void DostawaControl_Load(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                ContextInformation.Instance._context.Dostawas.Load();
+                dostawaBindingSource.DataSource = ContextInformation.Instance._context.Dostawas.Local.ToBindingList();
+            }
+        }
+
+        private void zatwierdzButton_Click(object sender, EventArgs e)
+        {
+            if(!DesignMode)
+            {
+                dostawaBindingSource.ResetBindings(false);
+                ContextInformation.Instance._context.SaveChanges();
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

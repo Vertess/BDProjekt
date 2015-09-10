0b5d29755e518baad24954342d5c9d4e7871cbf1
lwieserce <lwieserce114@gmail.com>
2015-09-10 19:32:40 +0200

    Added OfertyControl
    nie wiem czy wszystko dobrze jak coś to sprawdzcie + dodajcie do
    odpowiednich zakładek bo mi Szef nie pokazał jak xD

---
+++ BDProjekt/Controls/OfertyControl.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace BDProjekt.Controls
+{
+    partial class OfertyControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ofertaOferentaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.egzemplarzBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.zatwierdzButton = new System.Windows.Forms.Button();
+            this.oferentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idOfertyOferentaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cenaHurtowaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.egzemplarzDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.oferentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ofertaOferentaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.egzemplarzBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oferentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idOfertyOferentaDataGridViewTextBoxColumn,
+            this.cenaHurtowaDataGridViewTextBoxColumn,
+            this.egzemplarzDataGridViewTextBoxColumn,
+            this.oferentDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ofertaOferentaBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ofertaOferentaBindingSource
+            // 
+            this.ofertaOferentaBindingSource.DataSource = typeof(BDProjekt.OfertaOferenta);
+            // 
+            // egzemplarzBindingSource
+            // 
+            this.egzemplarzBindingSource.DataSource = typeof(BDProjekt.Egzemplarz);
+            // 
+            // zatwierdzButton
+            // 
+            this.zatwierdzButton.Location = new System.Drawing.Point(391, 230);
+            this.zatwierdzButton.Name = "zatwierdzButton";
+            this.zatwierdzButton.Size = new System.Drawing.Size(75, 23);
+            this.zatwierdzButton.TabIndex = 1;
+            this.zatwierdzButton.Text = "Zatwierdź";
+            this.zatwierdzButton.UseVisualStyleBackColor = true;
+            this.zatwierdzButton.Click += new System.EventHandler(this.zatwierdzButton_Click);
+            // 
+            // oferentBindingSource
+            // 
+            this.oferentBindingSource.DataSource = typeof(BDProjekt.Oferent);
+            // 
+            // idOfertyOferentaDataGridViewTextBoxColumn
+            // 
+            this.idOfertyOferentaDataGridViewTextBoxColumn.DataPropertyName = "IdOfertyOferenta";
+            this.idOfertyOferentaDataGridViewTextBoxColumn.HeaderText = "IdOfertyOferenta";
+            this.idOfertyOferentaDataGridViewTextBoxColumn.Name = "idOfertyOferentaDataGridViewTextBoxColumn";
+            // 
+            // cenaHurtowaDataGridViewTextBoxColumn
+            // 
+            this.cenaHurtowaDataGridViewTextBoxColumn.DataPropertyName = "CenaHurtowa";
+            this.cenaHurtowaDataGridViewTextBoxColumn.HeaderText = "CenaHurtowa";
+            this.cenaHurtowaDataGridViewTextBoxColumn.Name = "cenaHurtowaDataGridViewTextBoxColumn";
+            // 
+            // egzemplarzDataGridViewTextBoxColumn
+            // 
+            this.egzemplarzDataGridViewTextBoxColumn.DataSource = this.egzemplarzBindingSource;
+            this.egzemplarzDataGridViewTextBoxColumn.HeaderText = "Egzemplarz";
+            this.egzemplarzDataGridViewTextBoxColumn.Name = "egzemplarzDataGridViewTextBoxColumn";
+            this.egzemplarzDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.egzemplarzDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.egzemplarzDataGridViewTextBoxColumn.ValueMember = "IdEgzemplarza";
+            // 
+            // oferentDataGridViewTextBoxColumn
+            // 
+            this.oferentDataGridViewTextBoxColumn.DataSource = this.oferentBindingSource;
+            this.oferentDataGridViewTextBoxColumn.DisplayMember = "NazwaFirmy";
+            this.oferentDataGridViewTextBoxColumn.HeaderText = "Oferent";
+            this.oferentDataGridViewTextBoxColumn.Name = "oferentDataGridViewTextBoxColumn";
+            this.oferentDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.oferentDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.oferentDataGridViewTextBoxColumn.ValueMember = "IdOferenta";
+            // 
+            // OfertyControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.zatwierdzButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "OfertyControl";
+            this.Size = new System.Drawing.Size(470, 313);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ofertaOferentaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.egzemplarzBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oferentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource egzemplarzBindingSource;
+        private System.Windows.Forms.BindingSource ofertaOferentaBindingSource;
+        private System.Windows.Forms.Button zatwierdzButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idOfertyOferentaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cenaHurtowaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn egzemplarzDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn oferentDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource oferentBindingSource;
+    }
+}
---
+++ BDProjekt/Controls/OfertyControl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace BDProjekt.Controls
+{
+    public partial class OfertyControl : UserControl
+    {
+        public OfertyControl()
+        {
+            InitializeComponent();
+        }
+        private void KsiazkaControl_Load(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                ContextInformation.Instance._context.Egzemplarzs.Load();
+                egzemplarzBindingSource.DataSource = ContextInformation.Instance._context.Egzemplarzs.Local.ToBindingList();
+                ContextInformation.Instance._context.Oferents.Load();
+                oferentBindingSource.DataSource = ContextInformation.Instance._context.Oferents.Local.ToBindingList();
+                ContextInformation.Instance._context.OfertaOferentas.Load();
+                ofertaOferentaBindingSource.DataSource = ContextInformation.Instance._context.OfertaOferentas.Local.ToBindingList();
+            }
+        }
+
+        private void zatwierdzButton_Click(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                Egzemplarz eg = new Egzemplarz();
+                Oferent of = new Oferent();
+                OfertaOferenta oo = new OfertaOferenta();
+                eg = (Egzemplarz)egzemplarzBindingSource.Current;
+                of = (Oferent)oferentBindingSource.Current;
+                oo = (OfertaOferenta)ofertaOferentaBindingSource.Current;
+                oo.Oferent_IdOferenta = of.IdOferenta;
+                ofertaOferentaBindingSource.ResetBindings(false);
+                ContextInformation.Instance._context.SaveChanges();
+            }
+
+        }
+    }
+}

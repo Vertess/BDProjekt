61df87cc0e8bfab3a5e9e897ccc3ae468f31d8a8
lwieserce <lwieserce114@gmail.com>
2015-09-12 13:40:32 +0200

    Fix DostawaControl

--- BDProjekt/Controls/DostawaControl.Designer.cs
+++ BDProjekt/Controls/DostawaControl.Designer.cs
@@ -30,18 +30,18 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dostawaBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.oferentBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.pracownikBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.idDostawyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zatwierdzButton = new System.Windows.Forms.Button();
+            this.ElementyDostawies = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.oferentComboBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.pracownikComboBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.elementyDostawiesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.zatwierdzButton = new System.Windows.Forms.Button();
+            this.idDostawyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.oferentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pracownikBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dostawaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dostawaBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.oferentBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pracownikBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dostawaBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -50,32 +50,30 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.idDostawyDataGridViewTextBoxColumn,
+            this.ElementyDostawies,
             this.oferentComboBoxColumn,
-            this.pracownikComboBoxColumn,
-            this.elementyDostawiesDataGridViewTextBoxColumn});
+            this.pracownikComboBoxColumn});
             this.dataGridView1.DataSource = this.dostawaBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(20, 22);
+            this.dataGridView1.Location = new System.Drawing.Point(32, 18);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(444, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(447, 150);
             this.dataGridView1.TabIndex = 0;
             // 
-            // dostawaBindingSource
+            // zatwierdzButton
             // 
-            this.dostawaBindingSource.DataSource = typeof(BDProjekt.Dostawa);
+            this.zatwierdzButton.Location = new System.Drawing.Point(404, 174);
+            this.zatwierdzButton.Name = "zatwierdzButton";
+            this.zatwierdzButton.Size = new System.Drawing.Size(75, 23);
+            this.zatwierdzButton.TabIndex = 1;
+            this.zatwierdzButton.Text = "Zatwierdź";
+            this.zatwierdzButton.UseVisualStyleBackColor = true;
+            this.zatwierdzButton.Click += new System.EventHandler(this.zatwierdzButton_Click);
             // 
-            // oferentBindingSource
+            // ElementyDostawies
             // 
-            this.oferentBindingSource.DataSource = typeof(BDProjekt.Oferent);
-            // 
-            // pracownikBindingSource
-            // 
-            this.pracownikBindingSource.DataSource = typeof(BDProjekt.Pracownik);
-            // 
-            // idDostawyDataGridViewTextBoxColumn
-            // 
-            this.idDostawyDataGridViewTextBoxColumn.DataPropertyName = "IdDostawy";
-            this.idDostawyDataGridViewTextBoxColumn.HeaderText = "IdDostawy";
-            this.idDostawyDataGridViewTextBoxColumn.Name = "idDostawyDataGridViewTextBoxColumn";
+            this.ElementyDostawies.DataPropertyName = "ElementyDostawies";
+            this.ElementyDostawies.HeaderText = "ElementyDostawies";
+            this.ElementyDostawies.Name = "ElementyDostawies";
             // 
             // oferentComboBoxColumn
             // 
@@ -90,28 +88,30 @@
             // pracownikComboBoxColumn
             // 
             this.pracownikComboBoxColumn.DataSource = this.pracownikBindingSource;
-            this.pracownikComboBoxColumn.DisplayMember = "Login";
+            this.pracownikComboBoxColumn.DisplayMember = "Nazwisko";
             this.pracownikComboBoxColumn.HeaderText = "Pracownik";
             this.pracownikComboBoxColumn.Name = "pracownikComboBoxColumn";
             this.pracownikComboBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.pracownikComboBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             this.pracownikComboBoxColumn.ValueMember = "IdPracownika";
             // 
-            // elementyDostawiesDataGridViewTextBoxColumn
+            // idDostawyDataGridViewTextBoxColumn
             // 
-            this.elementyDostawiesDataGridViewTextBoxColumn.DataPropertyName = "ElementyDostawies";
-            this.elementyDostawiesDataGridViewTextBoxColumn.HeaderText = "ElementyDostawies";
-            this.elementyDostawiesDataGridViewTextBoxColumn.Name = "elementyDostawiesDataGridViewTextBoxColumn";
+            this.idDostawyDataGridViewTextBoxColumn.DataPropertyName = "IdDostawy";
+            this.idDostawyDataGridViewTextBoxColumn.HeaderText = "IdDostawy";
+            this.idDostawyDataGridViewTextBoxColumn.Name = "idDostawyDataGridViewTextBoxColumn";
             // 
-            // zatwierdzButton
+            // oferentBindingSource
             // 
-            this.zatwierdzButton.Location = new System.Drawing.Point(388, 179);
-            this.zatwierdzButton.Name = "zatwierdzButton";
-            this.zatwierdzButton.Size = new System.Drawing.Size(75, 23);
-            this.zatwierdzButton.TabIndex = 1;
-            this.zatwierdzButton.Text = "Zatwierdź";
-            this.zatwierdzButton.UseVisualStyleBackColor = true;
-            this.zatwierdzButton.Click += new System.EventHandler(this.zatwierdzButton_Click);
+            this.oferentBindingSource.DataSource = typeof(BDProjekt.Oferent);
+            // 
+            // pracownikBindingSource
+            // 
+            this.pracownikBindingSource.DataSource = typeof(BDProjekt.Pracownik);
+            // 
+            // dostawaBindingSource
+            // 
+            this.dostawaBindingSource.DataSource = typeof(BDProjekt.Dostawa);
             // 
             // DostawaControl
             // 
@@ -120,12 +120,12 @@
             this.Controls.Add(this.zatwierdzButton);
             this.Controls.Add(this.dataGridView1);
             this.Name = "DostawaControl";
-            this.Size = new System.Drawing.Size(466, 278);
+            this.Size = new System.Drawing.Size(488, 251);
             this.Load += new System.EventHandler(this.DostawaControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dostawaBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.oferentBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pracownikBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dostawaBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -133,13 +133,13 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDostawyDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewComboBoxColumn oferentComboBoxColumn;
         private System.Windows.Forms.BindingSource oferentBindingSource;
-        private System.Windows.Forms.DataGridViewComboBoxColumn pracownikComboBoxColumn;
         private System.Windows.Forms.BindingSource pracownikBindingSource;
-        private System.Windows.Forms.DataGridViewTextBoxColumn elementyDostawiesDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource dostawaBindingSource;
         private System.Windows.Forms.Button zatwierdzButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDostawyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ElementyDostawies;
+        private System.Windows.Forms.DataGridViewComboBoxColumn oferentComboBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn pracownikComboBoxColumn;
     }
 }
--- BDProjekt/Controls/DostawaControl.cs
+++ BDProjekt/Controls/DostawaControl.cs
@@ -18,28 +18,35 @@ namespace BDProjekt.Controls
             InitializeComponent();
         }
 
-        private void DostawaControl_Load(object sender, EventArgs e)
-        {
-            Funkcje.Instance._context.Dostawas.Load();
-            dostawaBindingSource.DataSource = Funkcje.Instance._context.Dostawas.Local.ToBindingList();
-            Funkcje.Instance._context.Oferents.Load();
-            oferentBindingSource.DataSource = Funkcje.Instance._context.Oferents.Local.ToBindingList();
-            Funkcje.Instance._context.Pracowniks.Load();
-            pracownikBindingSource.DataSource = Funkcje.Instance._context.Pracowniks.Local.ToBindingList();
-        }
-
         private void zatwierdzButton_Click(object sender, EventArgs e)
         {
-            Dostawa ds = new Dostawa();
-            Oferent of = new Oferent();
-            Pracownik pr = new Pracownik();
-            ds = (Dostawa)dostawaBindingSource.Current;
-            of = (Oferent)oferentBindingSource.Current;
-            pr = (Pracownik)pracownikBindingSource.Current;
-            ds.Oferent_IdOferenta = of.IdOferenta;
-            ds.Pracownik_IdPracownika = pr.IdPracownika;
-            dostawaBindingSource.ResetBindings(false);
-            Funkcje.Instance._context.SaveChanges();
+            if (!DesignMode)
+            {
+                Dostawa ds = new Dostawa();
+                Oferent of = new Oferent();
+                Pracownik pr = new Pracownik();
+                ds = (Dostawa)dostawaBindingSource.Current;
+                of = (Oferent)oferentBindingSource.Current;
+                pr = (Pracownik)pracownikBindingSource.Current;
+                ds.Oferent_IdOferenta = of.IdOferenta;
+                ds.Pracownik_IdPracownika = pr.IdPracownika;
+                dostawaBindingSource.ResetBindings(false);
+                Funkcje.Instance._context.SaveChanges();
+            }
+        }
+
+        private void DostawaControl_Load(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                Funkcje.Instance._context.Dostawas.Load();
+                dostawaBindingSource.DataSource = Funkcje.Instance._context.Dostawas.Local.ToBindingList();
+                Funkcje.Instance._context.Oferents.Load();
+                oferentBindingSource.DataSource = Funkcje.Instance._context.Oferents.Local.ToBindingList();
+                Funkcje.Instance._context.Pracowniks.Load();
+                pracownikBindingSource.DataSource = Funkcje.Instance._context.Pracowniks.Local.ToBindingList();
+            }
         }
     }
 }
+

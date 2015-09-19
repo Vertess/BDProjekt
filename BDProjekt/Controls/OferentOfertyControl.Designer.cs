978115400bb59b9c61c7a1088dc824a881febb83
dixiluk <lukaszdiekemper@gmail.com>
2015-09-19 20:50:44 +0200

    Prawie gotowa OfertyOferenta

--- BDProjekt/Controls/OferentOfertyControl.Designer.cs
+++ BDProjekt/Controls/OferentOfertyControl.Designer.cs
@@ -30,29 +30,29 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button2 = new System.Windows.Forms.Button();
             this.TytolKsiazki = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NazwaWydawnictwa = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Dodaj = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Ksiazka2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Wydawnictwo2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TypOkladki = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zatwierdzButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cenaHurtowaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idOfertyOferentaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Egzemplarz = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ofertaOferentaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.typOkladkiDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.IdEgzemplarza = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ksiazka = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Wydawnictwo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.egzemplarzBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.Ksiazka2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Wydawnictwo2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TypOkladki = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.cenaHurtowaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.idOfertyOferentaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Egzemplarz = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ofertaOferentaBindingSource)).BeginInit();
@@ -82,78 +82,6 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(74, 4);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(526, 281);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(306, 26);
-            this.textBox1.TabIndex = 1;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "Ksiazka";
-            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.DataPropertyName = "Ksiazka";
-            this.dataGridViewTextBoxColumn2.HeaderText = "Column1";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(121, 236);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 81);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "daj nazwy";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.AllowUserToAddRows = false;
-            this.dataGridView2.AutoGenerateColumns = false;
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Ksiazka2,
-            this.Wydawnictwo2,
-            this.TypOkladki,
-            this.cenaHurtowaDataGridViewTextBoxColumn,
-            this.idOfertyOferentaDataGridViewTextBoxColumn,
-            this.Egzemplarz});
-            this.dataGridView2.DataSource = this.ofertaOferentaBindingSource;
-            this.dataGridView2.Location = new System.Drawing.Point(567, 3);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowTemplate.Height = 28;
-            this.dataGridView2.Size = new System.Drawing.Size(649, 215);
-            this.dataGridView2.TabIndex = 3;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.DataPropertyName = "Egzemplarz";
-            this.dataGridViewTextBoxColumn3.HeaderText = "Egzemplarz";
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(325, 246);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 61);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "reload";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // TytolKsiazki
             // 
             this.TytolKsiazki.DataPropertyName = "int";
@@ -173,6 +101,118 @@
             this.Dodaj.HeaderText = "Dodaj";
             this.Dodaj.Name = "Dodaj";
             this.Dodaj.ReadOnly = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(74, 4);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Ksiazka";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Ksiazka";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ksiazka2,
+            this.Wydawnictwo2,
+            this.TypOkladki,
+            this.cenaHurtowaDataGridViewTextBoxColumn,
+            this.idOfertyOferentaDataGridViewTextBoxColumn,
+            this.Egzemplarz});
+            this.dataGridView2.DataSource = this.ofertaOferentaBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(567, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(609, 215);
+            this.dataGridView2.TabIndex = 3;
+            this.dataGridView2.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellDoubleClick);
+            // 
+            // Ksiazka2
+            // 
+            this.Ksiazka2.HeaderText = "Ksiazka";
+            this.Ksiazka2.Name = "Ksiazka2";
+            this.Ksiazka2.ReadOnly = true;
+            // 
+            // Wydawnictwo2
+            // 
+            this.Wydawnictwo2.HeaderText = "Wydawnictwo";
+            this.Wydawnictwo2.Name = "Wydawnictwo2";
+            this.Wydawnictwo2.ReadOnly = true;
+            // 
+            // TypOkladki
+            // 
+            this.TypOkladki.HeaderText = "TypOkladki";
+            this.TypOkladki.Name = "TypOkladki";
+            this.TypOkladki.ReadOnly = true;
+            this.TypOkladki.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.TypOkladki.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Egzemplarz";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Egzemplarz";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Visible = false;
+            // 
+            // zatwierdzButton
+            // 
+            this.zatwierdzButton.Location = new System.Drawing.Point(883, 224);
+            this.zatwierdzButton.Name = "zatwierdzButton";
+            this.zatwierdzButton.Size = new System.Drawing.Size(151, 84);
+            this.zatwierdzButton.TabIndex = 5;
+            this.zatwierdzButton.Text = "Przeladuj / Zatwierdz zmiany";
+            this.zatwierdzButton.UseVisualStyleBackColor = true;
+            this.zatwierdzButton.Click += new System.EventHandler(this.zatwierdzButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 254);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(364, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Filter:";
+            // 
+            // cenaHurtowaDataGridViewTextBoxColumn
+            // 
+            this.cenaHurtowaDataGridViewTextBoxColumn.DataPropertyName = "CenaHurtowa";
+            this.cenaHurtowaDataGridViewTextBoxColumn.HeaderText = "CenaHurtowa";
+            this.cenaHurtowaDataGridViewTextBoxColumn.Name = "cenaHurtowaDataGridViewTextBoxColumn";
+            // 
+            // idOfertyOferentaDataGridViewTextBoxColumn
+            // 
+            this.idOfertyOferentaDataGridViewTextBoxColumn.DataPropertyName = "IdOfertyOferenta";
+            this.idOfertyOferentaDataGridViewTextBoxColumn.HeaderText = "IdOfertyOferenta";
+            this.idOfertyOferentaDataGridViewTextBoxColumn.Name = "idOfertyOferentaDataGridViewTextBoxColumn";
+            this.idOfertyOferentaDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // Egzemplarz
+            // 
+            this.Egzemplarz.DataPropertyName = "Egzemplarz";
+            this.Egzemplarz.HeaderText = "Egzemplarz";
+            this.Egzemplarz.Name = "Egzemplarz";
+            this.Egzemplarz.Visible = false;
             // 
             // ofertaOferentaBindingSource
             // 
@@ -214,54 +254,14 @@
             // 
             this.egzemplarzBindingSource.DataSource = typeof(BDProjekt.Egzemplarz);
             // 
-            // Ksiazka2
-            // 
-            this.Ksiazka2.HeaderText = "Ksiazka";
-            this.Ksiazka2.Name = "Ksiazka2";
-            this.Ksiazka2.ReadOnly = true;
-            // 
-            // Wydawnictwo2
-            // 
-            this.Wydawnictwo2.HeaderText = "Wydawnictwo";
-            this.Wydawnictwo2.Name = "Wydawnictwo2";
-            this.Wydawnictwo2.ReadOnly = true;
-            // 
-            // TypOkladki
-            // 
-            this.TypOkladki.HeaderText = "TypOkladki";
-            this.TypOkladki.Name = "TypOkladki";
-            this.TypOkladki.ReadOnly = true;
-            this.TypOkladki.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.TypOkladki.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // cenaHurtowaDataGridViewTextBoxColumn
-            // 
-            this.cenaHurtowaDataGridViewTextBoxColumn.DataPropertyName = "CenaHurtowa";
-            this.cenaHurtowaDataGridViewTextBoxColumn.HeaderText = "CenaHurtowa";
-            this.cenaHurtowaDataGridViewTextBoxColumn.Name = "cenaHurtowaDataGridViewTextBoxColumn";
-            // 
-            // idOfertyOferentaDataGridViewTextBoxColumn
-            // 
-            this.idOfertyOferentaDataGridViewTextBoxColumn.DataPropertyName = "IdOfertyOferenta";
-            this.idOfertyOferentaDataGridViewTextBoxColumn.HeaderText = "IdOfertyOferenta";
-            this.idOfertyOferentaDataGridViewTextBoxColumn.Name = "idOfertyOferentaDataGridViewTextBoxColumn";
-            this.idOfertyOferentaDataGridViewTextBoxColumn.Visible = false;
-            // 
-            // Egzemplarz
-            // 
-            this.Egzemplarz.DataPropertyName = "Egzemplarz";
-            this.Egzemplarz.HeaderText = "Egzemplarz";
-            this.Egzemplarz.Name = "Egzemplarz";
-            this.Egzemplarz.Visible = false;
-            // 
             // OferentOfertyControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.zatwierdzButton);
             this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "OferentOfertyControl";
             this.Size = new System.Drawing.Size(2415, 518);
@@ -281,15 +281,12 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.DataGridViewTextBoxColumn egzemplarzDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource egzemplarzBindingSource;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn ksiazkaDataGridViewTextBoxColumn;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.BindingSource ofertaOferentaBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridViewTextBoxColumn TytolKsiazki;
         private System.Windows.Forms.DataGridViewTextBoxColumn NazwaWydawnictwa;
         private System.Windows.Forms.DataGridViewCheckBoxColumn typOkladkiDataGridViewCheckBoxColumn;
@@ -303,5 +300,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn cenaHurtowaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn idOfertyOferentaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn Egzemplarz;
+        private System.Windows.Forms.Button zatwierdzButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- BDProjekt/Controls/OferentOfertyControl.cs
+++ BDProjekt/Controls/OferentOfertyControl.cs
@@ -19,46 +19,7 @@ namespace BDProjekt.Controls
             InitializeComponent();
         }
 
-
-
-
-        private void OferentOfertyControl_Load(object sender, EventArgs e)
-        {
-            
-            using (var db = new KsiegarniaEntities())
-            {
-                try
-                {
-                    var oferty = from b in db.OfertaOferentas
-                                  select b;
-
-                }
-                catch (Exception)
-                {
-                    return ;
-                }
-            }
-
-            if (!DesignMode)
-            {
-                Funkcje.Instance._context.Egzemplarzs.Load();
-                egzemplarzBindingSource.DataSource = Funkcje.Instance._context.Egzemplarzs.Local.ToBindingList();
-                dataGridView1.Columns["NazwaWydawnictwa"].ValueType = typeof(string);
-                dataGridView1.Columns["TytolKsiazki"].ValueType = typeof(string);
-
-                Funkcje.Instance._context.OfertaOferentas.Load();
-                ofertaOferentaBindingSource.DataSource = Funkcje.Instance._context.OfertaOferentas.Local.ToBindingList();
-                dataGridView2.Columns["Ksiazka2"].ValueType = typeof(string);
-                dataGridView2.Columns["Wydawnictwo2"].ValueType = typeof(string);
-                dataGridView2.Columns["TypOkladki"].ValueType = typeof(bool);
-
-
-                    return;
-            }
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void DajNazwy()
         {
 
 
@@ -90,10 +51,15 @@ namespace BDProjekt.Controls
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.ColumnIndex == 3)
-            {
+            if (e.ColumnIndex == 3) {
+
                 try
                 {
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    if (((Egzemplarz)row.Cells["Egzemplarz"].Value).IdEgzemplarza == (int)dataGridView1.Rows[e.RowIndex].Cells["IdEgzemplarza"].Value)
+                        return;
+                }
                     using (var db = new KsiegarniaEntities())
                     {
                         Egzemplarz egzemplarz = ((Egzemplarz)egzemplarzBindingSource[0]);
@@ -106,25 +72,73 @@ namespace BDProjekt.Controls
 
                         db.OfertaOferentas.Add(oferta);
                         db.SaveChanges();
+                        reload(sender, e);
+                        return;
                     }
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
-                    textBox1.Text = ex.ToString();
                     return ;
                 }
             }
 
-
-
-
-
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+        private void dataGridView2_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            OferentOfertyControl_Load(sender, e);
+            if (e.ColumnIndex == 0)
+                try
+                {
+                    dataGridView2.Rows.Remove(dataGridView2.Rows[e.RowIndex]);
+                    reload(sender, e);
+                }
+                catch (Exception)
+                {
+                    return;
+                }
         }
+
+        private void reload(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                Funkcje.Instance._context.Egzemplarzs.Load();
+                egzemplarzBindingSource.DataSource = Funkcje.Instance._context.Egzemplarzs.Local.ToBindingList().Where(n => (((Ksiazka)n.Ksiazka).Tytul.Contains(this.textBox2.Text)));
+
+                Funkcje.Instance._context.OfertaOferentas.Load();
+                ofertaOferentaBindingSource.DataSource = Funkcje.Instance._context.OfertaOferentas.Local.ToBindingList();
+                DajNazwy();
+
+                return;
+            }
+        }
+
+        private void zatwierdzButton_Click(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+            ofertaOferentaBindingSource.ResetBindings(false);
+            Funkcje.Instance._context.SaveChanges();
+            reload(sender, e);
+
+                return;
+            }
+        }
+
+        private void OferentOfertyControl_Load(object sender, EventArgs e)
+        {
+
+            dataGridView2.Columns["Ksiazka2"].ValueType = typeof(string);
+            dataGridView2.Columns["Wydawnictwo2"].ValueType = typeof(string);
+            dataGridView2.Columns["TypOkladki"].ValueType = typeof(bool);
+            dataGridView1.Columns["NazwaWydawnictwa"].ValueType = typeof(string);
+            dataGridView1.Columns["TytolKsiazki"].ValueType = typeof(string);
+        }
+
+ 
+
+
 
  
 
